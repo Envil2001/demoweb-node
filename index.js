92f6e53997df5c07e1be316d2ec897c869734b92
Envil2001 <sherbakov12342@gmail.com>
2025-11-23 18:52:51 +0100

    add redis service and docker-compose setup

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const http = require("http");
+const { createClient } = require("redis");
+
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
+
+const client = createClient({
+	url: "redis://redis:6379"
+});
+
+client.on("error", (err) => console.error("Redis error", err));
+client.connect();
+
+const server = http.createServer(async (req, res) => {
+	const count = await client.incr("counter");
+
+	
 	res.writeHead(200, {"Content-Type": "text/html; charset=utf=8" });
-	res.end("<h1>Docker work!</h1>");
+	res.end(`<h1>Docker work!</h1> <p>Hit counter:  ${count}</p>`);
 });
 
 server.listen(PORT, () => {
